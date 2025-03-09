77eed1f6365b57fde599dfc4af4b645de107dba6
Ho Fung Tsoi <hftsoi0411@gmail.com>
2025-03-09 15:01:14 -0400

    py files

---
+++ build_model.py
@@ -0,0 +1,98 @@
+from tensorflow.keras.layers import *
+from tensorflow import keras
+from tensorflow.keras import layers, models, Model
+import tensorflow as tf
+import numpy as np
+
+
+def build_autoencoder(input_dim=57):
+    input_layer = layers.Input(shape=(input_dim,))
+    encoded = layers.Dense(32, activation='relu')(input_layer)
+    encoded = layers.Dense(16, activation='relu')(encoded)
+    latent = layers.Dense(3, activation='relu')(encoded)
+    
+    decoded = layers.Dense(16, activation='relu')(latent)
+    decoded = layers.Dense(32, activation='relu')(decoded)
+    output_layer = layers.Dense(input_dim, activation='relu')(decoded)
+    
+    model = Model(inputs=input_layer, outputs=output_layer)
+    model.compile(optimizer=tf.keras.optimizers.legacy.Adam(learning_rate=0.005), loss='mse')
+    return model
+
+
+class PlanarFlow(layers.Layer):
+    def __init__(self, **kwargs):
+        super(PlanarFlow, self).__init__(**kwargs)
+    
+    def build(self, input_shape):
+        self.input_dim = input_shape[-1]
+        self.w = self.add_weight(name='w', shape=(self.input_dim,),
+                                 initializer='random_normal', trainable=True)
+        self.u = self.add_weight(name='u', shape=(self.input_dim,),
+                                 initializer='random_normal', trainable=True)
+        self.b = self.add_weight(name='b', shape=(1,),
+                                 initializer='random_normal', trainable=True)
+        super(PlanarFlow, self).build(input_shape)
+    
+    def call(self, inputs):
+        # f(x) = x + u * h(w^T x + b)
+        linear_term = tf.tensordot(inputs, self.w, axes=1) + self.b
+        h = tf.tanh(linear_term)
+        outputs = inputs + tf.expand_dims(h, -1) * self.u
+
+        # log|det| = log|1 + u^T h'(w^T x + b) * w|
+        h_prime = 1 - tf.square(tf.tanh(linear_term))
+        psi = tf.expand_dims(h_prime, -1) * self.w
+        u_dot_psi = tf.reduce_sum(self.u * psi, axis=-1)
+        log_det = tf.math.log(tf.abs(1 + u_dot_psi) + 1e-5)
+
+        return outputs, log_det
+    
+
+class NormalizingFlowModel(Model):
+    def __init__(self, num_flows, **kwargs):
+        super(NormalizingFlowModel, self).__init__(**kwargs)
+        self.dense1 = layers.Dense(32, activation=layers.LeakyReLU(alpha=0.1))
+        self.dense2 = layers.Dense(16, activation=layers.LeakyReLU(alpha=0.1))
+        self.dense3 = layers.Dense(4, activation=layers.LeakyReLU(alpha=0.1))
+        self.flows = [PlanarFlow() for _ in range(num_flows)]
+    
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        x = self.dense2(x)
+        x = self.dense3(x)
+        log_det_total = 0
+
+        for flow in self.flows:
+            x, log_det = flow(x)
+            log_det_total += log_det
+
+        return x, log_det_total
+    
+
+def base_log_prob(z):
+    d = tf.cast(tf.shape(z)[-1], tf.float32)
+    log_prob = -0.5 * (tf.reduce_sum(tf.square(z), axis=-1) + d * tf.math.log(2 * np.pi))
+    return log_prob
+
+
+def nll_loss(model, x):
+    z, log_det_total = model(x)
+    log_prob = base_log_prob(z) + log_det_total
+    loss = -tf.reduce_mean(log_prob)
+    return loss
+
+
+def compute_mse_for_ae(model, x):
+    reco = model.predict(x)
+    mse = np.mean(np.square(x - reco), axis=1)
+    return mse
+
+
+def compute_nll_for_nf(model, data):
+    z, log_det_total = model(data)
+    log_prob = base_log_prob(z) + log_det_total
+    nll = -log_prob.numpy()
+    return nll
+
+
---
+++ utils.py
@@ -0,0 +1,48 @@
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_curve, auc
+
+
+def plot_data(data, obj):
+    if obj == 'MET':
+        k = 1
+    elif obj == 'Ele':
+        k = 2
+    elif obj == 'Mu':
+        k = 3
+    elif obj == 'Jet':
+        k = 4
+
+    plt.figure(figsize=(7,5))
+
+    for i in range(len(data)):
+        plt.hist(data[i][:,:,0][data[i][:,:,3]==k], log=True, label=files[i], alpha=1, histtype='step',
+                 density=True, bins=100)
+        plt.xlabel('Pt [GeV]')
+        plt.title(obj)
+        plt.legend()
+    plt.show()
+    
+    plt.figure(figsize=(7,5))
+    for i in range(len(data)):
+        plt.hist(data[i][:,:,1][data[i][:,:,3]==k], log=True, label=files[i], alpha=1, histtype='step',
+                 density=True, bins=100)
+        plt.xlabel('Eta')
+        plt.title(obj)
+        #plt.legend()
+    plt.show()
+    
+    plt.figure(figsize=(7,5))
+    for i in range(len(data)):
+        plt.hist(data[i][:,:,2][data[i][:,:,3]==k], log=True, label=files[i], alpha=1, histtype='step',
+                 density=True, bins=100)
+        plt.xlabel('Phi')
+        plt.title(obj)
+        #plt.legend()
+    plt.show()
+
+
